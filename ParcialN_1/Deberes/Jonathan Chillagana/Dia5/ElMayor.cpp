3ad8e173734e2069ff467ae291a58058e437a9bc
paul1897 <93349086+paul1897@users.noreply.github.com>
2021-11-02 19:48:13 -0500

    Add files via upload

---
+++ ParcialN_1/Deberes/Jonathan Chillagana/Dia5/ElMayor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "funcion.cpp"
+using namespace std;
+
+/***********************************************************************
+ * Universidad de las Fuerzas Armadas ESPE
+ * Numero Mayor
+ * Autor:   Jonathan Chillagana
+ * Fecha de creacion: Martes, Noviembre 2, 2021 7:0:30 PM
+ * Fecha de modificacion: Martes, Noviembre 2, 2021 7:25:10 PM
+ * NRC: 7167
+ * Nombre de la materia: Estructura de Datos
+ ***********************************************************************/
+ 
+int main() {
+	cout<< "El mayor de 2 numeros enteros es : "<< mayor(10,50)<<endl;
+	cout<< "El mayor de 2 numeros reales es: "<<mayor(9.81,3.14)<<endl;
+
+}
---
+++ ParcialN_1/Deberes/Jonathan Chillagana/Dia5/funcion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+template <class T>
+T mayor(T dato1, T dato2){
+	if(dato1>=dato2){
+		return dato1;
+	}
+	else{
+		return dato2;
+	}
+	
+}
