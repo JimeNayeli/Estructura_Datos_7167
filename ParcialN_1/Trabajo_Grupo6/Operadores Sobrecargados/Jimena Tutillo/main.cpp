79a29d8cc454af450a68ba2428720d8c843be3e2
JimenaNayeliTutillo <93277934+JimenaNayeliTutillo@users.noreply.github.com>
2021-11-04 21:37:37 -0500

    Update main.cpp

--- ParcialN_1/Trabajo_Grupo6/Operadores Sobrecargados/Jimena Tutillo/main.cpp
+++ ParcialN_1/Trabajo_Grupo6/Operadores Sobrecargados/Jimena Tutillo/main.cpp
@@ -5,7 +5,7 @@
  *Programa: Operadores Sobrecargados-Operador ++
  * Autor:Jimena Tutillo
  * Fecha de creacion: Jueves, Noviembre 4, 2021 8:49:23 PM
- * Fecha de modificacion: Noviembre 4, 29, 2021 9:40:23 PM
+ * Fecha de modificacion: Jueves, Noviembre 4, 29, 2021 9:40:23 PM
  * NRC: 7167
  * Nombre de la materia: Estructura de Datos
 
@@ -15,11 +15,11 @@ int main() {
    Tiempo T1(18, 59), T2(10,50);
    cout<<"\n\tTiempo T1"<<endl;
    for(int i=0;i<4;i++,T1++){
-   		T1.mostrarTiempo();
+   	T1.mostrarTiempo();
    }
    cout<<"\n\tTiempo T2"<<endl;
    for(int i=0;i<4;i++,T2++){
-   		T2.mostrarTiempo();
+   	T2.mostrarTiempo();
    }
 
    return 0;
