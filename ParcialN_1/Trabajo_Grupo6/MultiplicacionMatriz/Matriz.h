628fea288d97fefe464891cdcd78836ee28e507b
JimenaNayeliTutillo <93277934+JimenaNayeliTutillo@users.noreply.github.com>
2021-11-14 22:34:09 -0500

    Add files via upload

---
+++ ParcialN_1/Trabajo_Grupo6/MultiplicacionMatriz/Matriz.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+bool validar(int fila, int col);
+void ingresar(int **mat,int fila, int col);
+void procesar(int **mat1, int **mat2, int **matR, int fila,int col,int col2);
+
