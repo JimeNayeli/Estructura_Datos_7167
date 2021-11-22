022628bde274f74217daa072ed9b6801bcc7dfd6
JimenaNayeliTutillo <93277934+JimenaNayeliTutillo@users.noreply.github.com>
2021-11-21 22:55:37 -0500

    Update main.cpp

--- ParcialN_1/Trabajo_Grupo6/Nodos_Ejercicio/main.cpp
+++ ParcialN_1/Trabajo_Grupo6/Nodos_Ejercicio/main.cpp
@@ -1,12 +1,19 @@
+/***********************************************************************
+ * Universidad de las Fuerzas Armadas ESPE
+ * Programa: Listas 
+ * Autor:Paul Chillagana, Jorge Nacimba, Jimena Tutillo
+ * Fecha de creacion: Domingo, Noviembre 21, 2021 8:10:23 PM
+ * Fecha de modificacion: Domingo, Noviembre 21, 2021 10:50:23 PM
+ * NRC: 7167
+ * Nombre de la materia: Estructura de Datos
+ 
+***********************************************************************/
 #include <iostream>
 #include "Nodo.cpp"
 #include "Lista.cpp"
 #include "Menu.h"
 
 int main(int argc, char** argv) {
-	
-	Lista lista;
     menuPrincipal(); 
-
 	return 0;
 }
