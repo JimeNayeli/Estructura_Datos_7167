aa8c592df1085061f17dc8bf831f1bb8adf9977f
paul1897 <93349086+paul1897@users.noreply.github.com>
2021-11-23 23:16:30 -0500

    Update main.cpp

--- ParcialN_1/Trabajo_Grupo6/ListaSimplesCirculares/main.cpp
+++ ParcialN_1/Trabajo_Grupo6/ListaSimplesCirculares/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+/***********************************************************************
+ * Universidad de las Fuerzas Armadas ESPE
+ * Programa: Lista Circular Doblemente Enlazada
+ * Integrantes:  Paul Chillagana, Jorge Nasimba, Jimena Tutillo
+ * Fecha de creacion: Martes, Noviembre 23, 2021 7:50:23 PM
+ * Fecha de modificacion: Martes, Noviembre 23, 2021 11:10:58 PM
+ * NRC: 7167
+ * Nombre de la materia: Estructura de Datos
+ ***********************************************************************/
 
 #include "listaSimple.cpp"
 int main(int argc, char** argv) {
