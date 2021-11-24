97ffc8a7dbfa42d385a49377416bcdfc451d3fec
JimenaNayeliTutillo <93277934+JimenaNayeliTutillo@users.noreply.github.com>
2021-11-23 23:05:34 -0500

    Update main.cpp

--- ParcialN_1/Trabajo_Grupo6/ListaCircularDoble/main.cpp
+++ ParcialN_1/Trabajo_Grupo6/ListaCircularDoble/main.cpp
@@ -2,10 +2,10 @@
 #include "Menu.h"
 /***********************************************************************
  * Universidad de las Fuerzas Armadas ESPE
- * Plantilla de el ejercicio Fraccion
+ * Programa: Lista Circular Doblemente Enlazada
  * Integrantes:  Paul Chillagana, Jorge Nasimba, Jimena Tutillo
- * Fecha de creacion: Jueves, Noviembre 23, 2021 5:30:23 PM
- * Fecha de modificacion: Jueves, Noviembre 23, 2021 18:10:58 PM
+ * Fecha de creacion: Martes, Noviembre 23, 2021 5:30:23 PM
+ * Fecha de modificacion: Martes, Noviembre 23, 2021 18:10:58 PM
  * NRC: 7167
  * Nombre de la materia: Estructura de Datos
  ***********************************************************************/
