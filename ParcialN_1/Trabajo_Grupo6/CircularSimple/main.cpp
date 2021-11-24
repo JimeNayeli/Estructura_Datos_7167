20b08f645c1e6094ef1e59b6fa8425cee2bbb440
JimenaNayeliTutillo <93277934+JimenaNayeliTutillo@users.noreply.github.com>
2021-11-23 21:51:43 -0500

    Update main.cpp

--- ParcialN_1/Trabajo_Grupo6/CircularSimple/main.cpp
+++ ParcialN_1/Trabajo_Grupo6/CircularSimple/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "Menu.h"
+/***********************************************************************
+ * Universidad de las Fuerzas Armadas ESPE
+ * Programa: Lista Circular Simple
+ * Integrantes:  Paul Chillagana, Jorge Nasimba, Jimena Tutillo
+ * Fecha de creacion: Martes, Noviembre 23, 2021 5:30:23 PM
+ * Fecha de modificacion: Martes, Noviembre 23, 2021 18:10:58 PM
+ * NRC: 7167
+ * Nombre de la materia: Estructura de Datos
+ ***********************************************************************/
 
 int main() {
 	menuPrincipal();
-
-
-
 	return 0;
 }
